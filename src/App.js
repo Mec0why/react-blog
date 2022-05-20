78ddad3eade37a9e75df099c1824856631e6d7f0
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-05-20 17:39:47 +0200

    Add Routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+
 function App() {
-  return <main>Hello World!</main>;
+  return (
+    <main>
+      Hello World!
+      <Routes>
+        <Route path='/' element={'<Home />'} />
+        <Route path='/post/:id' element={'<Post />'} />
+        <Route path='/post/add' element={'<PostAdd />'} />
+        <Route exact strict path='/post/edit/:id' element={'<PostEdit />'} />
+        <Route path='/about' element={'<About />'} />
+        <Route path='*' element={'<NotFound />'} />
+      </Routes>
+    </main>
+  );
 }
 
 export default App;
