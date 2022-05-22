48f769abced7fbf79dfb67d4a9e8a022c5ccafbd
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-05-22 12:42:43 +0200

    Add initialState content

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,5 +1,38 @@
 const initialState = {
-  posts: [],
+  posts: [
+    {
+      id: '1',
+      title: 'Article title',
+      shortDescription: 'Short description of the article...',
+      content: 'Main content of the article',
+      publishedDate: '02-02-2022',
+      author: 'John Doe',
+    },
+    {
+      id: '2',
+      title: 'Article title',
+      shortDescription: 'Short description of the article...',
+      content: 'Main content of the article',
+      publishedDate: '12-02-2022',
+      author: 'Philip Anselmo',
+    },
+    {
+      id: '3',
+      title: 'Article title',
+      shortDescription: 'Short description of the article...',
+      content: 'Main content of the article',
+      publishedDate: '21-02-2022',
+      author: 'Geralt of Rivia',
+    },
+    {
+      id: '4',
+      title: 'Article title',
+      shortDescription: 'Short description of the article...',
+      content: 'Main content of the article',
+      publishedDate: '27-02-2022',
+      author: 'Vito Corleone',
+    },
+  ],
 };
 
 export default initialState;
